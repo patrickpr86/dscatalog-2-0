409124d0a9fd82ebdd5a1ea6e579b6e6747fc6f8
patrickpr86 <sh4dbr@gmail.com>
2021-04-22 15:34:44 -0300

    Implementing oauth2 authorization server

--- backend/src/main/java/com/patrick/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/patrick/dscatalog/config/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class AppConfig {
-	
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
--- backend/src/main/java/com/patrick/dscatalog/services/UserService.java
+++ backend/src/main/java/com/patrick/dscatalog/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,27 +30,29 @@ import com.patrick.dscatalog.services.exceptions.DatabaseException;
 import com.patrick.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
-	@Autowired
-	private UserRepository productRepository;
-
-	@Autowired
-	private RoleRepository roleRepository;
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private RoleRepository roleRepository;
+
 	@Transactional(readOnly = true)
 	public Page<UserDTO> findAllPaged(PageRequest pageRequest) {
-		Page<User> list = productRepository.findAll(pageRequest);
+		Page<User> list = userRepository.findAll(pageRequest);
 		return list.map(x -> new UserDTO(x));
 
 	}
 
 	@Transactional(readOnly = true)
 	public UserDTO findById(Long id) {
-		Optional<User> obj = productRepository.findById(id);
+		Optional<User> obj = userRepository.findById(id);
 		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new UserDTO(entity);
 	}
@@ -55,16 +62,16 @@ public class UserService {
 		User entity = new User();
 		copyDtoToEntity(dto, entity);
 		entity.setPassword(passwordEncoder.encode(dto.getPassword()));
-		entity = productRepository.save(entity);
+		entity = userRepository.save(entity);
 		return new UserDTO(entity);
 	}
 
 	@Transactional
 	public UserDTO update(Long id, UserUpdateDTO dto) {
 		try {
-			User entity = productRepository.getOne(id);
+			User entity = userRepository.getOne(id);
 			copyDtoToEntity(dto, entity);
-			entity = productRepository.save(entity);
+			entity = userRepository.save(entity);
 			return new UserDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
@@ -74,7 +81,7 @@ public class UserService {
 
 	public void delete(Long id) {
 		try {
-			productRepository.deleteById(id);
+			userRepository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 		} catch (DataIntegrityViolationException e) {
@@ -95,6 +102,18 @@ public class UserService {
 			entity.getRoles().add(role);
 		}
 
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		User user = userRepository.findByEmail(username);
+		if (user == null) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("email not found!");
+		}
+		logger.info("User found: " + username);		
+		return user;
 	}
 
 }
