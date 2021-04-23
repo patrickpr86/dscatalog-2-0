55ea760d4e31721a193ba749612b7d9ed82a2c6f
patrickpr86 <sh4dbr@gmail.com>
2021-04-23 10:53:19 -0300

    Add information on token with enhance method

---
+++ backend/src/main/java/com/patrick/dscatalog/component/JwtTokenEnhancer.java
@@ -0,0 +1,39 @@
+package com.patrick.dscatalog.component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.patrick.dscatalog.entities.User;
+import com.patrick.dscatalog.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenEnhancer implements TokenEnhancer {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+
+        User user = userRepository.findByEmail(authentication.getName());
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("userFirstName", user.getFirstName());
+        map.put("userId", user.getId());
+        map.put("userRole", user.getRoles());
+
+        DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) accessToken;
+        token.setAdditionalInformation(map);
+
+        return accessToken;
+
+    }
+
+}
--- backend/src/main/java/com/patrick/dscatalog/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/patrick/dscatalog/config/AuthorizationServerConfig.java
@@ -1,5 +1,9 @@
 package com.patrick.dscatalog.config;
 
+import java.util.Arrays;
+
+import com.patrick.dscatalog.component.JwtTokenEnhancer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +14,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
 
@@ -38,6 +43,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
+	@Autowired
+	private JwtTokenEnhancer tokenEnhancer;
+
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
@@ -55,8 +63,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		
+		TokenEnhancerChain chain = new TokenEnhancerChain();
+		chain.setTokenEnhancers(Arrays.asList(tokenEnhancer, accessTokenConverter));
+
 		endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore)
-				.accessTokenConverter(accessTokenConverter);
+				.accessTokenConverter(accessTokenConverter)
+				.tokenEnhancer(chain);
 	}
 
 }
